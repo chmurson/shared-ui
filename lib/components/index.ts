3b1ae7baeb055df52aea1480d5382cc54779a562
Krystian Fras <krystian@fras.me>
2025-04-06 17:45:35 +0200

    Fix css

--- lib/components/index.ts
+++ lib/components/index.ts
@@ -1,3 +1,7 @@
+import { isDarkMode, setColorMode, useIsDarkMode, useToggleColorMode } from "./DarkMode/utils";
+
 export * from "./Header";
 export * from "./DarkMode";
 export * from "./AppsSidebar";
+
+export { isDarkMode, useIsDarkMode, useToggleColorMode, setColorMode };
