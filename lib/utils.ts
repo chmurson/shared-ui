441e52bf91eaf0132578b02be47874345d278afe
Tomek Drwięga <tomusdrw@users.noreply.github.com>
2025-07-25 11:45:00 +0200

    feat: allow redirecting to beta (#14)
    * feat: allow redirecting to beta
    
    * fix: condition

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,21 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+/**
+ * By opting in by setting `localStorage.BETA_REDIRECT` the user
+ * can automatically be redirect to beta version of the tool.
+ */
+export function redirectToBeta(betaHost: string) {
+  try {
+    const shouldRedirectLocal = localStorage.getItem("BETA_REDIRECT");
+    if (shouldRedirectLocal === null) {
+      return;
+    }
+    const url = new URL(window.location.href);
+    url.host = betaHost;
+    window.location.href = url.toString();
+  } catch (e) {
+    console.warn(`Error redirecting to beta: ${e}`);
+  }
+}
